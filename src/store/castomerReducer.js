39a99d0ec2b2b719fa98dfe3f9be89d2afcbed89
Медведев Иван <ranzar2000@gmail.com>
2024-03-10 22:09:44 +0300

    fix typo "CASTOMER..."

--- src/store/castomerReducer.js
+++ src/store/castomerReducer.js
@@ -12,8 +12,8 @@ const defaultState = {
  */
 export const castomerReducer = (state = defaultState, action) => {
     switch (action.type) {
-        case "ADD_CASTOMER": {
-            return { ...state, cash: state.cash + action.payload };
+        case "ADD_CUSTOMER": {
+            return { ...state, customers: [...state.customers, action.payload] };
         }
 
         case "GET_CASTOMERS": {
